779ef44b33ef5e72a4d8eeb18af6b6b7b93cce8d
ramsestone <ramsestone13@gmail.com>
2026-05-21 17:02:15 -0600

    Cosas

--- app/src/main/java/juego/NaveEnemiga.java
+++ app/src/main/java/juego/NaveEnemiga.java
@@ -4,9 +4,9 @@ import edu.epromero.util.Destruible;
 import edu.epromero.util.Imagen;
 
 public abstract class NaveEnemiga extends ElementoGrafico implements Destruible, Dispara{
-    public String tipo;
-    public int resistencia;
-    public int puntos;
+    protected int puntosDeVida;
+    protected int valorEnPuntos;
+    protected String tipoNave;
 
     public NaveEnemiga(Imagen sprite, double anchoPantalla, double altoPantalla) {
         super(sprite, anchoPantalla, altoPantalla);
